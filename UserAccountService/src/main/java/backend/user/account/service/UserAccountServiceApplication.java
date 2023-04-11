4b6ea3f028ca2573bf97fd9482e55405a930dded
Bogdan Matioc <bogdanmatioc@gmail.com>
2023-04-11 20:24:46 +0300

    add UserAccountService start project template

---
+++ UserAccountService/src/main/java/backend/user/account/service/UserAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package backend.user.account.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAccountServiceApplication.class, args);
+    }
+
+}
